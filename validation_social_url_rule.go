29bf9ddd28afaf3697881276d338b4d6cb873142
Rafael Batista <maranata_rafa@hotmail.com>
2025-01-17 10:43:04 +0000

    FEAT: add SocialURLRule for validating social media URLs

---
+++ validation_social_url_rule.go
@@ -0,0 +1,21 @@
+package validation
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type SocialURLRule struct{}
+
+func (r *SocialURLRule) Validate(value string, fieldName string) error {
+
+	regex := `^https://[a-zA-Z0-9.-]+(/[a-zA-Z0-9._~:/?#@!$&'()*+,;=%-]*)?$`
+
+	re := regexp.MustCompile(regex)
+
+	if !re.MatchString(value) {
+		return fmt.Errorf(Err_PARAMETER_SHOULD_BE_SOCIAL_URL_INVALID.Error(), fieldName)
+	}
+
+	return nil
+}

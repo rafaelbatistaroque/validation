124039057935d9777dcb7ffe4615938ed4f08e8c
Rafael Batista <maranata_rafa@hotmail.com>
2025-01-17 10:42:43 +0000

    FIX: adjust LengthRule

--- validation_length.go
+++ validation_length.go
@@ -3,12 +3,25 @@ package validation
 import "fmt"
 
 type LengthRule struct {
-	Length int
+	Exactly int
+	Max     int
+	Min     int
 }
 
 func (r *LengthRule) Validate(value string, fieldName string) error {
-	if len(value) != r.Length {
-		return fmt.Errorf(Err_PARAMETER_SHOULD_HAVE_EXACTLY_CHARACTER.Error(), fieldName, r.Length)
+	length := len(value)
+
+	if length != r.Exactly {
+		return fmt.Errorf(Err_PARAMETER_SHOULD_HAVE_EXACTLY_CHARACTER.Error(), fieldName, r.Exactly)
 	}
+
+	if r.Min > 0 && length < r.Min {
+		return fmt.Errorf(Err_PARAMETER_LENGHT_MIN_INVALID.Error(), fieldName, r.Min)
+	}
+
+	if r.Max > 0 && length > r.Max {
+		return fmt.Errorf(Err_PARAMETER_LENGHT_MAX_INVALID.Error(), fieldName, r.Max)
+	}
+
 	return nil
 }

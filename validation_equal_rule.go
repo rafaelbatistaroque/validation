1485af4f3ef630df306d08145275c39fab51dd09
Rafael Batista <maranata_rafa@hotmail.com>
2025-02-07 23:13:18 +0000

    FIX: solve return error

--- validation_equal_rule.go
+++ validation_equal_rule.go
@@ -1,12 +1,14 @@
 package validation
 
+import "fmt"
+
 type EqualRule struct {
 	Equal string
 }
 
 func (r *EqualRule) Validate(value string, fieldName string) error {
 	if r.Equal != value {
-		return Err_PARAMETER_INVALID
+		return fmt.Errorf(Err_PARAMETER_INVALID.Error(), fieldName)
 	}
 
 	return nil
